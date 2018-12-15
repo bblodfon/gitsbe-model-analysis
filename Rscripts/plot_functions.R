65101c5caabc63039bd905f41315584d8c063e3d
john <bblodfon@gmail.com>
2018-12-15 16:25:11 +0100

    added new function for plotting multiple density plots

--- Rscripts/plot_functions.R
+++ Rscripts/plot_functions.R
@@ -6,7 +6,7 @@ make.color.bar.plot = function(color.vector, number.vector, title, xlab) {
 
 make.barplot.on.model.stats = function(model.synergies.stats, cell.line) {
   y.axis.values = pretty(model.synergies.stats)
-  bp = barplot(model.synergies.stats, col = "blue",
+  bp = barplot(model.synergies.stats, col = "cornflowerblue",
                ylim = c(0, max(y.axis.values) + 500), yaxt = "n",
                main = paste0("True Positive Synergy Predictions",
                              " (", cell.line, ")"),
@@ -20,29 +20,29 @@ make.barplot.on.model.stats = function(model.synergies.stats, cell.line) {
 make.barplot.on.synergy.subset.stats =
   function(synergy.subset.stats, threshold.for.subset.removal, bottom.margin,
            cell.line) {
-  # If the number of models that predicted a specific synergy set is less
-  # than the 'threshold.for.subset.removal' then discard it
-  synergy.subset.stats = synergy.subset.stats[
-    !synergy.subset.stats < threshold.for.subset.removal
-  ]
+    # If the number of models that predicted a specific synergy set is less
+    # than the 'threshold.for.subset.removal' then discard it
+    synergy.subset.stats = synergy.subset.stats[
+      !synergy.subset.stats < threshold.for.subset.removal
+    ]
 
-  # To fit in the names of the drug combinations, specify in inches the bottom
-  # margin of the plot depending on the maximum size of a synergy subset:
-  # size: 1 => bottom.margin = 4
-  # size: 2 => bottom.margin = 6
-  # size: 3 => bottom.margin = 9
-  # size: 4 => bottom.margin = 12, etc.
-  par(mar = c(bottom.margin, 4, 4, 2)) # c(bottom, left, top, right)
+    # To fit in the names of the drug combinations, specify in inches the bottom
+    # margin of the plot depending on the maximum size of a synergy subset:
+    # size: 1 => bottom.margin = 4
+    # size: 2 => bottom.margin = 6
+    # size: 3 => bottom.margin = 9
+    # size: 4 => bottom.margin = 12, etc.
+    par(mar = c(bottom.margin, 4, 4, 2)) # c(bottom, left, top, right)
 
-  y.axis.values = pretty(synergy.subset.stats)
-  bp = barplot(synergy.subset.stats, col = "green", space = 0.5, las = 2,
-               main = paste0("Model Synergy Predictions per Observed Synergy",
-                              " Subset", " (", cell.line, ")"),
-               ylab = "Number of models", yaxt = "n",
-               ylim = c(0, max(y.axis.values) + 500))
-  axis(2, at = y.axis.values, las = 1)
+    y.axis.values = pretty(synergy.subset.stats)
+    bp = barplot(synergy.subset.stats, col = "green", space = 0.5, las = 2,
+                 main = paste0("Model Synergy Predictions per Observed Synergy",
+                                " Subset", " (", cell.line, ")"),
+                 ylab = "Number of models", yaxt = "n",
+                 ylim = c(0, max(y.axis.values) + 500))
+    axis(2, at = y.axis.values, las = 1)
 
-  add.numbers.above.the.bars(synergy.subset.stats, bp, color = "red")
+    add.numbers.above.the.bars(synergy.subset.stats, bp, color = "red")
 }
 
 add.numbers.above.the.bars = function(table.stats, bp, color) {
@@ -50,4 +50,15 @@ add.numbers.above.the.bars = function(table.stats, bp, color) {
     text(x = bp[i], y = table.stats[i], labels = table.stats[i],
          col = color, pos = 3)
   }
+}
+
+make.multiple.density.plot = function(densities, xlab) {
+  plot(NA, xlim = range(sapply(densities, "[", "x")),
+       ylim = range(sapply(dens, "[", "y")),
+       main = "Density Estimation for the Average State Difference",
+       xlab = "Activity state (absolute difference value)",
+       ylab = "Density")
+  mapply(lines, densities, col = 1:length(densities))
+
+  legend("topright", legend = names(densities), fill = 1:length(densities))
 }
