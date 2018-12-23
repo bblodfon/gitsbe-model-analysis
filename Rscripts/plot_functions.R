f5eca38a5abc9d30c95135cd20612330d7df778a
john <bblodfon@gmail.com>
2018-12-23 13:09:32 +0100

    refactoring and generalizing `make.barplot.on.models.stats` function

--- Rscripts/plot_functions.R
+++ Rscripts/plot_functions.R
@@ -1,28 +1,67 @@
-# Don't use it to visualize more than 50 colors
+# Don't use this function to visualize more than 50 colors
 make.color.bar.plot = function(color.vector, number.vector, title, xlab = "") {
   bp = barplot(rep(1,length(color.vector)), col = color.vector,
                axes = FALSE, xlab = xlab, main = title, border = NA)
   axis(1, bp, number.vector)
 }
 
-make.barplot.on.model.stats = function(model.synergies.stats, cell.line) {
-  y.axis.values = pretty(model.synergies.stats)
-  bp = barplot(model.synergies.stats, col = "cornflowerblue",
-               ylim = c(0, max(y.axis.values) + 500), yaxt = "n",
-               main = paste0("True Positive Synergy Predictions",
-                             " (", cell.line, ")"),
-               xlab = "Number of maximum correctly predicted synergies",
-               ylab = "Number of models")
-  axis(2, at = y.axis.values, las = 1)
+# models.stats is the result of `table(num.vector)`
+make.barplot.on.models.stats =
+  function(models.stats, cell.line, title, xlab, ylab, cont.values = FALSE) {
 
-  add.numbers.above.the.bars(model.synergies.stats, bp, color = "red")
+    # Find is there is just one `NaN` category
+    there.is.one.NaN.category = FALSE
+    nan.index = which(is.nan(as.numeric(names(models.stats))))
+    if (length(nan.index) == 1) {
+      there.is.one.NaN.category = TRUE
+      nan.value = models.stats[nan.index]
+    }
+
+    # If there is just one `NaN` category, put it first in the `models.stats`
+    # for presentation purposes in the barplot
+    if (there.is.one.NaN.category) {
+      models.stats = c(nan.value, models.stats[names(models.stats) != "NaN"])
+    }
+
+    x.axis.values =
+      get.x.axis.values(models.stats, there.is.one.NaN.category, cont.values)
+    y.axis.values = pretty(models.stats)
+
+    bp = barplot(models.stats, col = "cornflowerblue",
+                 names.arg = x.axis.values, yaxt = "n",
+                 ylim = c(0, max(y.axis.values) + 500),
+                 main = paste0(title, " (", cell.line, ")"),
+                 xlab = xlab, ylab = ylab)
+    axis(2, at = y.axis.values, las = 1)
+
+    add.numbers.above.the.bars(models.stats, bp, color = "red")
+
+    # If there is just one `NaN` category, label it in the plot
+    if (there.is.one.NaN.category) {
+      text(x = bp[1], y = nan.value/2, labels = names(nan.value),
+           col = "yellow", srt = 90, font = 2)
+    }
+}
+
+get.x.axis.values =
+  function(models.stats, there.is.one.NaN.category, cont.values) {
+    if (there.is.one.NaN.category) {
+      # replace `NaN` value with empty space at the beginning of the x axis
+      x.values = c(" ", names(models.stats)[names(models.stats) != "NaN"])
+    } else x.values = names(models.stats)
+
+    if (cont.values) {
+      return(round(as.numeric(x.values), digits = 3))
+    } else {
+      return(x.values)
+    }
 }
 
 make.barplot.on.synergy.subset.stats =
   function(synergy.subset.stats, threshold.for.subset.removal, bottom.margin,
            cell.line) {
     # If the number of models that predicted a specific synergy set is less
-    # than the 'threshold.for.subset.removal' then discard it
+    # than the `threshold.for.subset.removal` then discard it
     synergy.subset.stats = synergy.subset.stats[
       !synergy.subset.stats < threshold.for.subset.removal
     ]
@@ -46,14 +85,14 @@ make.barplot.on.synergy.subset.stats =
     add.numbers.above.the.bars(synergy.subset.stats, bp, color = "red")
 }
 
-add.numbers.above.the.bars = function(table.stats, bp, color) {
-  for (i in 1:length(table.stats)) {
-    text(x = bp[i], y = table.stats[i], labels = table.stats[i],
+add.numbers.above.the.bars = function(stats, bp, color) {
+  for (i in 1:length(stats)) {
+    text(x = bp[i], y = stats[i], labels = stats[i],
          col = color, pos = 3)
   }
 }
 
-make.multiple.density.plot = function(densities, xlab) {
+make.multiple.density.plot = function(densities, legend.title) {
   plot(NA, xlim = range(sapply(densities, "[", "x")),
        ylim = range(sapply(densities, "[", "y")),
        main = "Density Estimation for the Average State Difference",
@@ -61,10 +100,11 @@ make.multiple.density.plot = function(densities, xlab) {
        ylab = "Density")
   mapply(lines, densities, col = 1:length(densities))
 
-  legend("topright", legend = names(densities), fill = 1:length(densities))
+  legend("topright", legend = names(densities), fill = 1:length(densities),
+         title = legend.title)
 }
 
-plot.network = function(net, diff, layout) {
+plot.network = function(net, diff, layout, title) {
   num.of.intervals = 2000
   colors = c("tomato", "grey", "gold")
 
@@ -92,8 +132,8 @@ plot.network = function(net, diff, layout) {
   V(net)$color = diff.colors[node.names]
 
   # plot the network
-  par(mar = c(0, 0, 0, 0))
-  plot(net, asp = 0, layout = layout)
+  par(mar = c(0, 0, 1, 0)) # c(bottom, left, top, right)
+  plot(net, asp = 0, layout = layout, main = title)
   legend(x = -1.1, y = -0.7, pch = 21, col = "#777777",
         legend = c("More inhibited","No difference", "More activated"),
         title = expression(bold("Good model activity state")),
