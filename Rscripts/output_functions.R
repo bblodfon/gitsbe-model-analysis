b13ee60819dc32bc37f6b444dd46c245b107e471
john <bblodfon@gmail.com>
2019-02-01 19:57:32 +0100

    added additional methods to update the biomarker files

--- Rscripts/output_functions.R
+++ Rscripts/output_functions.R
@@ -225,33 +225,67 @@ save.df.to.file = function(df, file) {
               row.names = TRUE, sep = "\t")
 }
 
-# we keep the biomarkers already stored in the respective file
-# and just add the new ones
+# We get the (previously-found) biomarkers from the respective file.
+# There are 3 policies regarding what to do with the 'new' biomarkers
+# when they share common nodes with the 'old' biomarkers. These are
+# given by the `method`:
+# 1) `replace` (default): if there is at least one new biomarker
+# common with the previously found ones, we discard the old ones and
+# write to the file the new biomarkers
+# 2) `prune.to.common`: keep only the common biomarkers
+# 3) `extend`: we add the non-common biomarkers to the old ones
+#
+# Note: No matter the `method`, if there are no common biomarkers
+# between old and new, we just add the new ones
 update.biomarkers.files =
   function(biomarkers.dir, drug.comb, biomarkers.active.new,
-           biomarkers.inhibited.new) {
+           biomarkers.inhibited.new, method = "replace") {
     # update the active biomarkers
     active.biomarkers.file =
       paste0(biomarkers.dir, drug.comb, "_biomarkers_active")
 
     if (file.size(active.biomarkers.file) == 0) {
       save.vector.to.file(vector = biomarkers.active.new,
-                          file = active.biomarkers.file, with.row.names = TRUE)
+                          file = active.biomarkers.file,
+                          with.row.names = TRUE)
     } else {
       biomarkers.active.prev =
         read.table(active.biomarkers.file, stringsAsFactors = FALSE)
       biomarkers.active.prev.names = biomarkers.active.prev[,1]
       biomarkers.active.new.names = names(biomarkers.active.new)
 
-      biomarkers.active.to.add = biomarkers.active.new[
-        !(biomarkers.active.new.names %in% biomarkers.active.prev.names)
-      ]
+      biomarkers.active.common = intersect(biomarkers.active.prev.names,
+                                           biomarkers.active.new.names)
 
-      biomarkers.active = add.vector.to.data.frame(biomarkers.active.prev,
-                                                   biomarkers.active.to.add)
-      biomarkers.active = transform(biomarkers.active, V2 = as.numeric(V2))
-      save.vector.to.file(vector = biomarkers.active,
-                          file = active.biomarkers.file)
+      if (length(biomarkers.active.common) == 0) {
+        biomarkers.active = add.vector.to.data.frame(biomarkers.active.prev,
+                                                     biomarkers.active.new)
+        biomarkers.active = transform(biomarkers.active, V2 = as.numeric(V2))
+        save.vector.to.file(vector = biomarkers.active,
+                            file = active.biomarkers.file)
+      } else {
+        if (method == "replace") {
+          biomarkers.active = biomarkers.active.new
+          save.vector.to.file(vector = biomarkers.active,
+                              file = active.biomarkers.file,
+                              with.row.names = TRUE)
+        } else if (method == "extend") {
+          # find the non-common biomarkers and add them to the 'old' ones
+          biomarkers.active.to.add = biomarkers.active.new[
+            !(biomarkers.active.new.names %in% biomarkers.active.prev.names)
+          ]
+          biomarkers.active = add.vector.to.data.frame(biomarkers.active.prev,
+                                                       biomarkers.active.to.add)
+          biomarkers.active = transform(biomarkers.active, V2 = as.numeric(V2))
+          save.vector.to.file(vector = biomarkers.active,
+                              file = active.biomarkers.file)
+        } else if (method == "prune.to.common") {
+          biomarkers.active = biomarkers.active.new[biomarkers.active.common]
+          save.vector.to.file(vector = biomarkers.active,
+                              file = active.biomarkers.file,
+                              with.row.names = TRUE)
+        }
+      }
     }
 
     # update the inhibited biomarkers
@@ -260,22 +294,46 @@ update.biomarkers.files =
 
     if (file.size(inhibited.biomarkers.file) == 0) {
       save.vector.to.file(vector = biomarkers.inhibited.new,
-                          file = inhibited.biomarkers.file, with.row.names = TRUE)
+                          file = inhibited.biomarkers.file,
+                          with.row.names = TRUE)
     } else {
       biomarkers.inhibited.prev =
         read.table(inhibited.biomarkers.file, stringsAsFactors = FALSE)
       biomarkers.inhibited.prev.names = biomarkers.inhibited.prev[,1]
       biomarkers.inhibited.new.names = names(biomarkers.inhibited.new)
 
-      biomarkers.inhibited.to.add = biomarkers.inhibited.new[
-        !(biomarkers.inhibited.new.names %in% biomarkers.inhibited.prev.names)
-      ]
+      biomarkers.inhibited.common = intersect(biomarkers.inhibited.prev.names,
+                                              biomarkers.inhibited.new.names)
 
-      biomarkers.inhibited = add.vector.to.data.frame(biomarkers.inhibited.prev,
-                                                      biomarkers.inhibited.to.add)
-      biomarkers.inhibited = transform(biomarkers.inhibited, V2 = as.numeric(V2))
-      save.vector.to.file(vector = biomarkers.inhibited,
-                          file = inhibited.biomarkers.file)
+      if (length(biomarkers.inhibited.common) == 0) {
+        biomarkers.inhibited = add.vector.to.data.frame(biomarkers.inhibited.prev,
+                                                        biomarkers.inhibited.new)
+        biomarkers.inhibited = transform(biomarkers.inhibited, V2 = as.numeric(V2))
+        save.vector.to.file(vector = biomarkers.inhibited,
+                            file = inhibited.biomarkers.file)
+      } else {
+        if (method == "replace") {
+          biomarkers.inhibited = biomarkers.inhibited.new
+          save.vector.to.file(vector = biomarkers.inhibited,
+                              file = inhibited.biomarkers.file,
+                              with.row.names = TRUE)
+        } else if (method == "extend") {
+          # find the non-common biomarkers and add them to the 'old' ones
+          biomarkers.inhibited.to.add = biomarkers.inhibited.new[
+            !(biomarkers.inhibited.new.names %in% biomarkers.inhibited.prev.names)
+          ]
+          biomarkers.inhibited = add.vector.to.data.frame(biomarkers.inhibited.prev,
+                                                          biomarkers.inhibited.to.add)
+          biomarkers.inhibited = transform(biomarkers.inhibited, V2 = as.numeric(V2))
+          save.vector.to.file(vector = biomarkers.inhibited,
+                              file = inhibited.biomarkers.file)
+        } else if (method == "prune.to.common") {
+          biomarkers.inhibited = biomarkers.inhibited.new[biomarkers.inhibited.common]
+          save.vector.to.file(vector = biomarkers.inhibited,
+                              file = inhibited.biomarkers.file,
+                              with.row.names = TRUE)
+        }
+      }
     }
 }
 
