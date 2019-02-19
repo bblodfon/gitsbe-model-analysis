71ab1dd91da78f95775547e71d9a5947da0a9975
john <bblodfon@gmail.com>
2019-02-19 18:33:08 +0100

    added external argument for rendering all of the cell-line specific notebooks

--- render_to_HTML.sh
+++ render_to_HTML.sh
@@ -3,7 +3,23 @@
 # finds all R notenooks (.Rmd) in current dir
 # and renders them to HTML
 
-rmd_files=`ls | grep Rmd`
+if [ "$#" != 1 ]; then
+  echo "Give one parameter: 'all' or 'some'!"
+  exit 1
+else
+  arg=$1
+fi
+
+if [ $arg == "all" ]; then
+  rmd_files=`ls | grep Rmd`
+elif [ $arg == "some" ]; then
+  rmd_files=`ls | grep Rmd | grep -v biomarker | grep -v consensus | grep -v fitness | grep -v prolif`
+else
+  echo "Write: 'all' or 'some'!"
+  exit 1
+fi
+
+echo $rmd_files
 
 for file in ${rmd_files}; do
   echo "Rendering "$file"..."
