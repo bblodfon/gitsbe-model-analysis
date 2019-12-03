5e2cf1f1a8881cd727474c28bc616a128f6295e2
john <bblodfon@gmail.com>
2019-12-03 14:46:55 +0100

    added render script for cascade synergy biomarkers rmarkdown document

---
+++ cascade/cell-lines-2500/render_csb.sh
@@ -0,0 +1,2 @@
+Rscript -e "rmarkdown::render(input = 'cascade_synergy_biomarkers.Rmd', output_dir = '../../docs/cascade/cell-lines-2500/')"
+Rscript -e "utils::browseURL(url = '/home/john/repos/druglogics/gitsbe-model-analysis/docs/cascade/cell-lines-2500/cascade_synergy_biomarkers.html')"
